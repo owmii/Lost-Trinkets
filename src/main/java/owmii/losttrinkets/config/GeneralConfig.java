bd17e4b622bf4ecf1f8ebe0c34e86d07790dac9a
BlueAgent <BlueAgent@users.noreply.github.com>
2021-01-14 15:03:18 +0800

    Add config for non-random trinkets
    Partially addresses #22

--- src/main/java/owmii/losttrinkets/config/GeneralConfig.java
+++ src/main/java/owmii/losttrinkets/config/GeneralConfig.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class GeneralConfig {
     public final ForgeConfigSpec.BooleanValue unlockEnabled;
     public final ForgeConfigSpec.ConfigValue<List<String>> blackList;
+    public final ForgeConfigSpec.ConfigValue<List<String>> nonRandom;
 
     public final ForgeConfigSpec.IntValue startSlots;
     public final ForgeConfigSpec.IntValue slotCost;
@@ -35,9 +36,13 @@ public class GeneralConfig {
 
         this.unlockEnabled = builder.comment("Set to false to disable the default way of unlocking trinkets.").define("unlockEnabled", true);
         this.blackList = builder
-                .comment("List of trinkets blocked from being unlocked, eg: [\"losttrinkets:piggy\", \"losttrinkets:magical_feathers\"]"
+                .comment("List of banned trinkets eg: [\"losttrinkets:piggy\", \"losttrinkets:magical_feathers\"]"
                         , "The trinkets listed in here will also be removed from players that already unlocked them.")
                 .define("blackList", Lists.newArrayList());
+        this.nonRandom = builder
+                .comment("List of trinkets that can't be unlocked randomly eg: [\"losttrinkets:piggy\", \"losttrinkets:magical_feathers\"]"
+                        , "The trinkets listed in here will not be removed from players that already unlocked them.")
+                .define("nonRandom", Lists.newArrayList());
 
         builder.push("Killing_Unlocks");
         this.killingUnlockEnabled = builder.comment("Set to false to disable unlocking trinkets from killing non-Boss entities.").define("killingUnlockEnabled", true);
