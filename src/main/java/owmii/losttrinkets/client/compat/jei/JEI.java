255c7863de044e8240a7f225ede8c070af687182
BlueAgent <BlueAgent@users.noreply.github.com>
2021-01-25 15:30:59 +0800

    Fix #40 by not trying to format the translation key

--- src/main/java/owmii/losttrinkets/client/compat/jei/JEI.java
+++ src/main/java/owmii/losttrinkets/client/compat/jei/JEI.java
@@ -4,7 +4,6 @@ import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeRegistration;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Util;
@@ -16,7 +15,7 @@ import owmii.losttrinkets.handler.UnlockManager;
 public class JEI implements IModPlugin {
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
-        UnlockManager.getTrinkets().stream().map(ItemStack::new).forEach(stack -> registration.addIngredientInfo(stack, VanillaTypes.ITEM, I18n.format(Util.makeTranslationKey("info", Registry.ITEM.getKey(stack.getItem())))));
+        UnlockManager.getTrinkets().stream().map(ItemStack::new).forEach(stack -> registration.addIngredientInfo(stack, VanillaTypes.ITEM, Util.makeTranslationKey("info", Registry.ITEM.getKey(stack.getItem()))));
     }
 
     @Override
