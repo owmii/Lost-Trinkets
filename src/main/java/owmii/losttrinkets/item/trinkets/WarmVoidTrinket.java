13e25f8d3c59060457666245db074c885cd3a0f8
BlueAgent <BlueAgent@users.noreply.github.com>
2021-03-15 23:57:14 +0800

    Fix Warm Void crash and spawn location

--- src/main/java/owmii/losttrinkets/item/trinkets/WarmVoidTrinket.java
+++ src/main/java/owmii/losttrinkets/item/trinkets/WarmVoidTrinket.java
@@ -1,14 +1,22 @@
 package owmii.losttrinkets.item.trinkets;
 
+import net.minecraft.command.impl.SpawnPointCommand;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.RegistryKey;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.util.ITeleporter;
 import owmii.losttrinkets.api.trinket.ITickableTrinket;
 import owmii.losttrinkets.api.trinket.Rarity;
 import owmii.losttrinkets.api.trinket.Trinket;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 public class WarmVoidTrinket extends Trinket<WarmVoidTrinket> implements ITickableTrinket {
     public WarmVoidTrinket(Rarity rarity, Properties properties) {
@@ -17,24 +25,46 @@ public class WarmVoidTrinket extends Trinket<WarmVoidTrinket> implements ITickab
 
     @Override
     public void tick(World world, BlockPos pos, PlayerEntity player) {
-        if (player.getPosY() <= -64) {
-            if (world instanceof ServerWorld && !player.isPassenger() && !player.isBeingRidden()) {
-                if (world.getDimensionKey() != World.OVERWORLD) {
-                    ServerWorld serverworld = ((ServerWorld) world).getServer().getWorld(World.OVERWORLD);
-                    if (serverworld != null) {
-                        player.changeDimension(serverworld);
-                    }
-                } else {
-                    Optional<BlockPos> position = player.getBedPosition();
-                    if (position.isPresent()) {
-                        BlockPos bedPos = position.get();
-                        player.setPositionAndUpdate(bedPos.getX(), bedPos.getY(), bedPos.getZ());
-                    } else {
-                        BlockPos spawnPos = ((ServerWorld) world).getSpawnPoint();
-                        player.setPositionAndUpdate(spawnPos.getX(), spawnPos.getY(), spawnPos.getZ());
-                    }
+        // TODO 1.17: Update -64
+        if (player instanceof ServerPlayerEntity && player.getPosY() + Math.min(0, player.getMotion().getY()) <= -64) {
+            if (!player.isPassenger() && !player.isBeingRidden()) {
+                teleportToSpawnPoint((ServerPlayerEntity) player);
+            }
+        }
+    }
+
+    /**
+     * Follow code in {@link SpawnPointCommand} to find the real spawn point information.
+     */
+    private static void teleportToSpawnPoint(ServerPlayerEntity player) {
+        ServerWorld currentWorld = player.getServerWorld();
+        RegistryKey<World> spawnDimensionKey = player.func_241141_L_();
+        if (currentWorld.getDimensionKey() != spawnDimensionKey) {
+            MinecraftServer server = currentWorld.getServer();
+            ServerWorld spawnWorld = server.getWorld(spawnDimensionKey);
+            if (spawnWorld != null) {
+                player.changeDimension(spawnWorld, new WarmVoidTeleporter());
+            }
+        } else {
+            Vector3d spawnPos = Vector3d.copyCenteredHorizontally(Optional.ofNullable(player.func_241140_K_())
+                    .orElseGet(currentWorld::getSpawnPoint));
+            float spawnAngle = player.func_242109_L();
+            player.connection.setPlayerLocation(spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), spawnAngle, 0);
+        }
+    }
+
+    public static class WarmVoidTeleporter implements ITeleporter {
+        @Override
+        public Entity placeEntity(Entity entity, ServerWorld currentWorld, ServerWorld destWorld, float yaw, Function<Boolean, Entity> repositionEntity) {
+            Entity maybePlayer = ITeleporter.super.placeEntity(entity, currentWorld, destWorld, yaw, repositionEntity);
+            if (maybePlayer instanceof ServerPlayerEntity) {
+                ServerPlayerEntity player = (ServerPlayerEntity) maybePlayer;
+                // Only try teleport if the destination matches spawn point
+                if (destWorld.getDimensionKey() == player.func_241141_L_()) {
+                    teleportToSpawnPoint(player);
                 }
             }
+            return maybePlayer;
         }
     }
 }
